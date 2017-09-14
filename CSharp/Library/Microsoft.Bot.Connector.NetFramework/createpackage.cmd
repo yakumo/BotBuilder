d92d0965fb6417d865168a29fb3e251df596f6f0
Akira Tomita <tomita@nttsolmare.com>
2017-09-14 13:33:18 +0900

    fixed packaging settings

--- CSharp/Library/Microsoft.Bot.Connector.NetFramework/createpackage.cmd
+++ CSharp/Library/Microsoft.Bot.Connector.NetFramework/createpackage.cmd
@@ -5,8 +5,7 @@ setlocal enableextensions
 set errorlevel=0
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Bot.Connector*nupkg
-msbuild /property:Configuration=release /p:DefineConstants="NET45" Microsoft.Bot.Connector.csproj 
-msbuild /property:Configuration=release ..\Microsoft.Bot.Connector.NetCore\Microsoft.Bot.Connector.NetCore.csproj 
-dotnet build --configuration Release ..\Microsoft.Bot.Connector.AspNetCore\project.json --no-dependencies
+dotnet msbuild /property:Configuration=release Microsoft.Bot.Connector.csproj 
+rem dotnet build --configuration Release ..\Microsoft.Bot.Connector.AspNetCore\project.json --no-dependencies
 for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Connector.dll).FileVersionInfo.FileVersion"') do set version=%%v
-..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Connector.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
+dotnet pack -c Release --no-build --include-source --include-symbols
