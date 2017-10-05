659a556753dc4cbde1957bfb33d2d882fdcb9ffd
Akira Tomita <tomita@nttsolmare.com>
2017-10-05 10:51:29 +0900

    fixed make package for .Net Standard 2.0

--- CSharp/Library/Microsoft.Bot.Builder/createpackage.cmd
+++ CSharp/Library/Microsoft.Bot.Builder/createpackage.cmd
@@ -7,11 +7,19 @@ set errorlevel=0
 mkdir ..\nuget
 erase /s ..\nuget\Microsoft.Bot.Connector*.nupkg
 erase /s ..\nuget\Microsoft.Bot.Builder*.nupkg
-msbuild /property:Configuration=release /p:DefineConstants="NET45" ..\Microsoft.Bot.Connector.NetFramework\Microsoft.Bot.Connector.csproj 
-msbuild /property:Configuration=release ..\Microsoft.Bot.Builder.Autofac\Microsoft.Bot.Builder.Autofac.csproj 
-msbuild /property:Configuration=release Microsoft.Bot.Builder.csproj 
+rem msbuild /property:Configuration=release /p:DefineConstants="NET45" ..\Microsoft.Bot.Connector.NetFramework\Microsoft.Bot.Connector.csproj 
+dotnet restore ..\Microsoft.Bot.Connector.NetCore\Microsoft.Bot.Connector.NetCore.csproj
+dotnet build -c Release ..\Microsoft.Bot.Connector.NetCore\Microsoft.Bot.Connector.NetCore.csproj
+dotnet restore ..\Microsoft.Bot.Connector.AspNetCore\Microsoft.Bot.Connector.AspNetCore.csproj
+dotnet build -c Release ..\Microsoft.Bot.Connector.AspNetCore\Microsoft.Bot.Connector.AspNetCore.csproj
+rem msbuild /property:Configuration=release ..\Microsoft.Bot.Builder.Autofac\Microsoft.Bot.Builder.Autofac.csproj 
+dotnet restore ..\Microsoft.Bot.Builder.Autofac\Microsoft.Bot.Builder.Autofac.csproj
+dotnet build -c Release ..\Microsoft.Bot.Builder.Autofac\Microsoft.Bot.Builder.Autofac.csproj
+rem msbuild /property:Configuration=release Microsoft.Bot.Builder.csproj 
+dotnet restore Microsoft.Bot.Builder.csproj
+dotnet build -c Release Microsoft.Bot.Builder.csproj
 msbuild /property:Configuration=release ..\..\tools\rview\rview.csproj
-for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set version=%%v
+for /f %%v in ('powershell -noprofile "(Get-Command .\bin\release\net46\Microsoft.Bot.Builder.dll).FileVersionInfo.FileVersion"') do set version=%%v
 ..\..\packages\NuGet.CommandLine.4.1.0\tools\NuGet.exe pack Microsoft.Bot.Builder.nuspec -symbols -properties version=%version% -OutputDirectory ..\nuget
 echo *** Finished building Microsoft.Bot.Builder
 
