3ae7ac8e844bb5051e92592154ad61d351f49f96
Akira Tomita <tomita@nttsolmare.com>
2017-10-12 11:32:05 +0900

    fixed bot authentication code for ASP.Net Core 2

--- CSharp/Library/Microsoft.Bot.Connector.AspNetCore/BotAuthentication2.cs
+++ CSharp/Library/Microsoft.Bot.Connector.AspNetCore/BotAuthentication2.cs
@@ -1,39 +1,42 @@
-﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace Microsoft.Bot.Connector
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
-    public class BotAuthentication : ActionFilterAttribute
+    /// <summary>
+    /// Bot authenticate for RECEIVED Activity
+    /// </summary>
+    /// <example>
+    /// [BotAuthentication]
+    /// [HttpPost]
+    /// public async Task Post([FromBody]Activity activity)
+    /// </example>
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class BotAuthentication : AuthorizeAttribute, IActionFilter
     {
-        /// <summary>
-        /// Microsoft AppId for the bot 
-        /// </summary>
-        /// <remarks>
-        /// Needs to be used with MicrosoftAppPassword.  Ignored if CredentialProviderType is specified.
-        /// </remarks>
-        public string MicrosoftAppId { get; set; }
+        public BotAuthentication()
+        {
+            AuthenticationSchemes = BotAuthenticationAppBuilderExtensions.DefaultBotAuthenticationScheme;
+        }
 
-        /// <summary>
-        /// Microsoft AppPassword for the bot (needs to be used with MicrosoftAppId)
-        /// </summary>
-        /// <remarks>
-        /// Needs to be used with MicrosoftAppId. Ignored if CredentialProviderType is specified.
-        /// </remarks>
-        public string MicrosoftAppPassword { get; set; }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
 
-        public string AuthenticationScheme { get; set; } = JwtBearerDefaults.AuthenticationScheme;
-
-        public bool DisableEmulatorTokens { get; set; }
-
-        public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        public void OnActionExecuting(ActionExecutingContext context)
         {
             var activities = GetActivities(context);
 
@@ -41,32 +44,9 @@ namespace Microsoft.Bot.Connector
             {
                 MicrosoftAppCredentials.TrustServiceUrl(activity.ServiceUrl);
             }
-
-            string appId = MicrosoftAppId ?? Environment.GetEnvironmentVariable(MicrosoftAppCredentials.MicrosoftAppIdKey, EnvironmentVariableTarget.Process);
-            string pass = MicrosoftAppPassword ?? Environment.GetEnvironmentVariable(MicrosoftAppCredentials.MicrosoftAppPasswordKey, EnvironmentVariableTarget.Process);
-            if (String.IsNullOrWhiteSpace(appId) || String.IsNullOrWhiteSpace(pass))
-            {
-                context.Result = new UnauthorizedResult();
-            }
-            else
-            {
-
-                string token = String.Empty;
-                string authorization = context.HttpContext.Request.Headers["Authorization"];
-                token = authorization?.Substring("Bearer ".Length).Trim();
-
-                var authenticator = new BotAuthenticator(MicrosoftAppId, MicrosoftAppPassword);
-                var authorized = await authenticator.TryAuthenticateAsync(AuthenticationScheme, token, CancellationToken.None);
-                if (!authorized.Authenticated)
-                {
-                    context.Result = new UnauthorizedResult();
-                }
-            }
-
-            await base.OnActionExecutionAsync(context, next);
         }
 
-        private IList<Activity> GetActivities(ActionExecutingContext actionContext)
+        public static IList<Activity> GetActivities(ActionExecutingContext actionContext)
         {
             var activties = actionContext.ActionArguments.Select(t => t.Value).OfType<Activity>().ToList();
             if (activties.Any())
@@ -88,6 +68,156 @@ namespace Microsoft.Bot.Connector
                 }
             }
             return activties;
+        }
+    }
+
+    /// <summary>
+    /// Extension methods to add BotAuthentication capabilities to an HTTP service collection.
+    /// </summary>
+    /// <example>
+    /// public void ConfigureServices(IServiceCollection services)
+    /// {
+    ///     services.AddAuthentication().AddBot("{AppId}", "{AppPassword}");
+    /// }
+    /// </example>
+    public static class BotAuthenticationAppBuilderExtensions
+    {
+        public const string DefaultBotAuthenticationScheme = "Bot";
+        public const string BotClaim = "Bot";
+
+        public static AuthenticationBuilder AddBot(this AuthenticationBuilder authBuilder, string microsoftAppId = null, string microsoftAppPassword = null)
+        {
+            if (String.IsNullOrWhiteSpace(microsoftAppId))
+            {
+                microsoftAppId = Environment.GetEnvironmentVariable(MicrosoftAppCredentials.MicrosoftAppIdKey, EnvironmentVariableTarget.Process);
+            }
+            if (String.IsNullOrWhiteSpace(microsoftAppPassword))
+            {
+                microsoftAppPassword = Environment.GetEnvironmentVariable(MicrosoftAppCredentials.MicrosoftAppPasswordKey, EnvironmentVariableTarget.Process);
+            }
+
+            return authBuilder.AddBot(new StaticCredentialProvider(microsoftAppId, microsoftAppPassword));
+        }
+
+        public static AuthenticationBuilder AddBot(this AuthenticationBuilder authBuilder, ICredentialProvider credentialProvider)
+        {
+            return authBuilder.AddBot(new BotAuthenticationOptions(credentialProvider));
+        }
+
+        public static AuthenticationBuilder AddBot(this AuthenticationBuilder authBuilder, BotAuthenticationOptions options)
+        {
+            authBuilder.Services.AddSingleton(typeof(BotAuthenticationOptions), options);
+            var ret = authBuilder.AddScheme<BotAuthenticationOptions, BotAuthenticationHandler>(BotAuthenticationAppBuilderExtensions.DefaultBotAuthenticationScheme, (conf) =>
+            {
+                conf.CredentialProvider = options.CredentialProvider;
+                conf.OpenIdConfiguration = options.OpenIdConfiguration;
+                conf.DisableEmulatorTokens = options.DisableEmulatorTokens;
+                conf.SaveToken = options.SaveToken;
+            });
+            return ret;
+        }
+    }
+
+    /// <summary>
+    /// Uses Authentication for Bot Connector
+    /// </summary>
+    public interface IBotAuthenticationOptions
+    {
+        ICredentialProvider CredentialProvider { set; get; }
+        string OpenIdConfiguration { set; get; }
+        bool DisableEmulatorTokens { set; get; }
+        bool SaveToken { set; get; }
+    }
+
+    /// <summary>
+    /// implementation to Authentication for Bot Connector
+    /// </summary>
+    public class BotAuthenticationOptions : AuthenticationSchemeOptions, IBotAuthenticationOptions
+    {
+        public BotAuthenticationOptions()
+        {
+        }
+
+        public BotAuthenticationOptions(ICredentialProvider credentialProvider)
+        {
+            CredentialProvider = credentialProvider;
+        }
+
+        /// <summary>
+        /// The <see cref="ICredentialProvider"/> used for authentication.
+        /// </summary>
+        public ICredentialProvider CredentialProvider { set; get; }
+
+        /// <summary>
+        /// The OpenId configuation.
+        /// </summary>
+        public string OpenIdConfiguration { set; get; } = JwtConfig.ToBotFromChannelOpenIdMetadataUrl;
+
+        /// <summary>
+        /// Flag indicating if emulator tokens should be disabled.
+        /// </summary>
+        public bool DisableEmulatorTokens { set; get; } = false;
+
+        /// <summary>
+        /// Flag indicating if <see cref="BotAuthenticationHandler"/> should be stored in 
+        /// the returned <see cref="Microsoft.AspNetCore.Authentication.AuthenticationTicket"/>. 
+        /// </summary>
+        public bool SaveToken { set; get; } = true;
+    }
+
+    /// <summary>
+    /// Authentication Handler
+    /// </summary>
+    public class BotAuthenticationHandler : AuthenticationHandler<BotAuthenticationOptions>
+    {
+        public BotAuthenticationHandler(IOptionsMonitor<BotAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock) : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            if (await Options.CredentialProvider.IsAuthenticationDisabledAsync())
+            {
+                var principal = new ClaimsPrincipal(new ClaimsIdentity(new Claim[] { new Claim(ClaimTypes.Role, BotAuthenticationAppBuilderExtensions.BotClaim) }));
+                return AuthenticateResult.Success(new AuthenticationTicket(principal, new AuthenticationProperties(), JwtBearerDefaults.AuthenticationScheme));
+            }
+
+            string token = null;
+
+            string authorization = Request.Headers["Authorization"];
+            token = authorization?.Substring("Bearer ".Length).Trim();
+
+            // If no token found, no further work possible
+            // and Authentication is not disabled fail
+            if (string.IsNullOrEmpty(token))
+            {
+                return AuthenticateResult.Fail("No JwtToken is present and BotAuthentication is enabled!");
+            }
+
+            var authenticator = new BotAuthenticator(Options.CredentialProvider, Options.OpenIdConfiguration, Options.DisableEmulatorTokens);
+            var identityToken = await authenticator.TryAuthenticateAsync(JwtBearerDefaults.AuthenticationScheme, token, CancellationToken.None);
+
+            if (identityToken.Authenticated)
+            {
+                identityToken.Identity.AddClaim(new Claim(ClaimTypes.Role, BotAuthenticationAppBuilderExtensions.BotClaim));
+                var principal = new ClaimsPrincipal(identityToken.Identity);
+                var ticket = new AuthenticationTicket(principal, new AuthenticationProperties(), JwtBearerDefaults.AuthenticationScheme);
+                Context.User = principal;
+
+                if (Options.SaveToken)
+                {
+                    ticket.Properties.StoreTokens(new[]
+                            {
+                                new AuthenticationToken { Name = "access_token", Value = token }
+                            });
+                }
+
+                return AuthenticateResult.Success(ticket);
+            }
+            else
+            {
+                return AuthenticateResult.Fail($"Failed to authenticate JwtToken {token}");
+            }
         }
     }
 }
